69c871f86e716a62df72f7002e24c529830d0d4b
YenYooo <point211@gmail.com>
2025-08-14 14:44:56 +0800

    feat(auth): implement Google login API in usersController

--- controllers/users.js
+++ controllers/users.js
@@ -20,6 +20,7 @@ const generateJWT = require('../utils/generateJWT');
 const { sendResetEmail } = require('../utils/mailer');
 const { nanoid } = require('nanoid');
 const crypto = require('crypto');
+const { OAuth2Client } = require('google-auth-library');
 // const { password } = require('../config/db');
 const { generateNewebpayForm } = require('../utils/newebpay/generateNewebpayForm');
 const { decryptTradeInfo, verifyNewebpaySignature } = require('../utils/newebpay/neWebPayCrypto');
@@ -200,6 +201,67 @@ const usersController = {
     } catch (error) {
       logger.error('登入錯誤:', error);
       next(error);
+    }
+  },
+
+  // Google 登入
+  async googleLogin(req, res) {
+    try {
+      const { credential: accessToken } = req.body;
+      const client = new OAuth2Client();
+
+      // 從 Google API 拿使用者資料（email、name）
+      const userInfoRes = await client.getTokenInfo(accessToken);
+      const email = userInfoRes.email;
+      const name = email.split('@')[0]; // 後端沒有拿到 name，就用 email 前段代替
+
+      const userRepository = dataSource.getRepository('User');
+      const existingUser = await userRepository.findOne({
+        select: ['id', 'email', 'password', 'name', 'role'],
+        where: { email },
+      });
+
+      if (existingUser && existingUser.password) {
+        return res.status(403).json({ message: '此信箱已註冊，請使用一般登入' });
+      }
+
+      let user;
+      if (!existingUser) {
+        user = userRepository.create({
+          email,
+          name,
+          password: null,
+          role: 'USER',
+        });
+        user = await userRepository.save(user);
+      } else {
+        user = existingUser;
+      }
+
+      const token = await generateJWT(
+        {
+          id: user.id,
+          role: user.role,
+        },
+        config.get('secret.jwtSecret'),
+        {
+          expiresIn: `${config.get('secret.jwtExpiresDay')}`,
+        }
+      );
+
+      res.status(201).json({
+        message: '登入成功',
+        data: {
+          token,
+          user: {
+            name: user.name,
+            role: user.role,
+          },
+        },
+      });
+    } catch (error) {
+      logger.error('Google 登入錯誤:', error);
+      res.status(401).json({ message: 'Google 驗證失敗' });
     }
   },
 
