36a82a4d5d0330397a6d1b5765ca189e90a07939
YenYooo <point211@gmail.com>
2025-08-14 14:36:18 +0800

    feat(auth): install google-auth-library and add GOOGLE_CLIENT_ID to env

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,9 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresDay: process.env.JWT_EXPIRES_DAY,
+  google: {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+  },
   // firebase: {
   //   serviceAccount: JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT),
   //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET
